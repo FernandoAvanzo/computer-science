8a78a574094d722515c767a468e20f4a3958f631
fernandoavanzo <fernando.avanzo@gmail.com>
2024-08-08 19:48:16 -0300

    refactor code

--- graphard/src/java/src/prog/Main.java
+++ graphard/src/java/src/prog/Main.java
@@ -13,36 +13,35 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import operacoes.BFS;
 import operacoes.BfsSaida;
-import operacoes.EC;
 import operacoes.MST;
 import operacoes.SCC;
 import operacoes.TS;
 
 
 public class Main {
-    public static void main(String [] args)throws Exception{
-       String sel = "ec";
-        
+    public static void main(String [] args) {
+       String sel = args[0];
+
         switch (sel) {
         case "bfs":
             BFS(args[1]);
             break;
-        
+
         case "ts" :
             TS(args[1]);
             break;
 
         case "scc":
             SCC(args[1]);
-            break; 
-                    
+            break;
+
         case "mst":
             MST(args[1]);
-        
+
         case "ec":
             System.out.println("rodou");
-            EC("~/Projects/computer-science/graphard/testes/ec/10.g");
-               
+            EC();
+
         default: break;
         }
     }
@@ -54,8 +53,8 @@ public class Main {
         G = bfs.getGbfs();
         BfsSaida out = new BfsSaida();
         for (int j = 0; j < G.getVSize(); j++) {
-          Vertice u = G.getV(j);
-          out.setAcessa(G.getV(j).getNome());
+            G.getV(j);
+            out.setAcessa(G.getV(j).getNome());
           out.setDistancia(G.getV(j).getD());
           out.setOrigem(G.getV(0).getNome()); 
           System.out.println(out.getOrigem()+" "+out.getAcessa()+" "+out.getDistancia());   
@@ -75,37 +74,36 @@ public class Main {
    }
 
     private static void SCC(String path) {
-            Grafo G = new Grafo(path);
-            Grafo GT = new Grafo(path);
+        new Grafo(path);
+        Grafo GT = new Grafo(path);
             SCC scc = new SCC(GT);
             ArrayList<Componente> comp = scc.getListSCC();
             Componente C;
-            
-            for (int i = 0; i < comp.size(); i++) {
-                C = comp.get(i);
-                for (int j = 0; j < C.componentSize(); j++) {
-                    Vertice u = C.getIten(j);
-                    System.out.print(u.getNome()+" ");
-                }
-                System.out.println("");
+
+        for (Componente componente : comp) {
+            C = componente;
+            for (int j = 0; j < C.componentSize(); j++) {
+                Vertice u = C.getIten(j);
+                System.out.print(u.getNome() + " ");
+            }
+            System.out.println();
         }
     }
 
     private static void MST(String path) {
         Grafo G = new Grafo(path);
-        Aresta e = G.getArestaE(0);
+        G.getArestaE(0);
         MST mst = new MST(G);
         LinkedList<Aresta> es = mst.getA();
-         for (int j = 0; j < es.size(); j++) {
-             Aresta w = es.get(j);
-             System.out.print(w.getOrigem().getNome()+" "+w.getDestino().getNome());
-             System.out.println("");
+        for (Aresta w : es) {
+            System.out.print(w.getOrigem().getNome() + " " + w.getDestino().getNome());
+            System.out.println();
 
-         }
+        }
     }
     
-    private static void EC(String path){
-        Tgf file = new Tgf(path);
+    private static void EC(){
+        Tgf file = new Tgf("~/Projects/computer-science/graphard/testes/ec/10.g");
         Grafo G = file.getG();
         //EC ec= new EC(G);
        
